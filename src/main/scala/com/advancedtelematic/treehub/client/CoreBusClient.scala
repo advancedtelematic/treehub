ae4da4114a701dcc32054bb08a34e15cd1af8019
Daniel Gustafsson <daniel@advancedtelematic.com>
2017-03-07 15:42:31 +0100

    Don't have a size of -1, instead have 0 for now

--- src/main/scala/com/advancedtelematic/treehub/client/CoreBusClient.scala
+++ src/main/scala/com/advancedtelematic/treehub/client/CoreBusClient.scala
@@ -17,7 +17,7 @@ class CoreBusClient(messageBusPublisher: MessageBusPublisher, treeHubUri: Uri) e
   private def mkTreehubCommit(ref: Ref, description: String): TreehubCommit = {
     //TODO: PRO-1802 pass the refname as the description until we can parse the real description out of the commit
     val formattedRefName = ref.name.get.replaceFirst("^heads/", "").replace("/", "-")
-    val size = -1 // TODO
+    val size = 0 // TODO
     TreehubCommit(ref.namespace, ref.value.get, formattedRefName, description, size, treeHubUri.toString)
   }
 
